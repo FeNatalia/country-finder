68f21c53545a56ac0e8f8f86e12dd5f9d14f787d
FeNatalia <nataliafedorova1989@gmail.com>
2022-03-17 22:10:52 +0100

    add a map, edit styles

--- client/src/pages/Details.jsx
+++ client/src/pages/Details.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 export const Details = () => {
   let { state } = useLocation();
@@ -11,13 +12,28 @@ export const Details = () => {
       <header className='details__header'>
         <img src={state.country.flags.png} alt={state.country.name.common} />
         <h1>{state.country.name.common}</h1>
-        <h3>Official name: {state.country.name.official}</h3>
       </header>
+      <div id="map">
+        <MapContainer center={state.country.latlng} zoom={6} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={state.country.latlng}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
       <section className='details__body'>
-        <p><strong>Region: </strong>{state.country.region}</p>
-        <p><strong>Subregion: </strong>{state.country.subregion}</p>
-        <p><strong>Population: </strong>{state.country.population}</p>
+        <p>Official name: <strong>{state.country.name.official}</strong></p>
+        <p>Region: <strong>{state.country.region}</strong></p>
+        <p>Subregion: <strong>{state.country.subregion}</strong></p>
+        <p>Country Population: <strong>{state.country.population}</strong></p>
+        <p>Capital: <strong>{state.country.capital}</strong></p>
       </section>
+
       <div className='details___button'>
         <button className="btn--primary" onClick={ () =>goBackButton()}>
           Go back
