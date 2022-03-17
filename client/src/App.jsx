8ced009449bfda10dca5b9d4f3bfafffd6d720aa
FeNatalia <nataliafedorova1989@gmail.com>
2022-03-17 01:09:31 +0100

    test to see if backend is connected

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
+import { useState, useEffect } from 'react';
+
 function App() {
+const [data, setData] = useState(null);
+
+useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+
   return (
     <div className="App">
       <h1>My App</h1>
+      <p>{!data ? "Loading..." : data}</p>
     </div>
   );
 }
