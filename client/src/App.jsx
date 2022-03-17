e5ec0ec960f20671f33aa0696c8d87387d9395ff
FeNatalia <nataliafedorova1989@gmail.com>
2022-03-17 10:53:46 +0100

    test connecting to backend and fetch all countries

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,23 @@
 import { useState, useEffect } from 'react';
+import { CountryDetail } from './components/CountryDetail';
 
 function App() {
-const [data, setData] = useState(null);
+const [data, setData] = useState([]);
 
 useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
-      .then((data) => setData(data.message));
+      .then((data) => {
+        setData(data)
+      });
   }, []);
 
-
+  const AllCountries = data.map((country) => <CountryDetail key={country.name.common} title={country.name.common} image={country.flags.png}/>)
+  
   return (
     <div className="App">
       <h1>My App</h1>
-      <p>{!data ? "Loading..." : data}</p>
+      {AllCountries}
     </div>
   );
 }
